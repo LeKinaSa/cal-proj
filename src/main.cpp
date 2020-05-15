0d2e3beb2498470829f6aba57983b264b9cec763
Goncalo Pascoal <goncalojpascoal@gmail.com>
2020-05-15 17:47:32 +0100

    Added function signature for mmpMethod and completed initReportGraph

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include "Graph.h"
 
-Graph<char> initReportGraph() {
-    Graph<char> graph;
+#include <vector>
 
+void initReportGraph(Graph<char>& graph, std::vector<Vertex<char>*>& pointsOfInterest, std::vector<double>& scores) {
     for (char c = 'a'; c <= 'k'; ++c) {
         graph.addVertex(c);
     }
@@ -38,11 +38,37 @@ Graph<char> initReportGraph() {
     graph.addEdge('s', 'c', 5.0);
     graph.addEdge('s', 'd', 3.0);
 
-    return graph;
+    pointsOfInterest.push_back(graph.findVertex('b'));
+    pointsOfInterest.push_back(graph.findVertex('h'));
+    pointsOfInterest.push_back(graph.findVertex('i'));
+    pointsOfInterest.push_back(graph.findVertex('j'));
+
+    scores.push_back(1.0);
+    scores.push_back(2.0);
+    scores.push_back(3.0);
+    scores.push_back(4.0);
+}
+
+
+template<class T>
+std::vector<Vertex<T>*> mmpMethod(
+        Graph<T>& graph,
+        const std::vector<Vertex<T>*>& pointsOfInterest,
+        const std::vector<double>& scores,
+        T start,
+        T finish,
+        double budget
+        ) {
+
+    return std::vector<Vertex<T>*>();
 }
 
 int main() {
-    Graph<char> graph = initReportGraph();
+    Graph<char> graph;
+    std::vector<Vertex<char>*> pointsOfInterest;
+    std::vector<double> scores;
+
+    initReportGraph(graph, pointsOfInterest, scores);
 
     return 0;
 }
