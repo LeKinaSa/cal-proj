7345044ca14154e798315eaabb51d45338ec050c
LeKinaSa <50105043+CAMartins@users.noreply.github.com>
2020-05-17 11:02:43 +0100

    Implementation of the redution step with Dijkstra method

--- src/Graph.h
+++ src/Graph.h
@@ -111,7 +111,7 @@ public:
 
     void dijkstraShortestPath(const T& source);
 
-    std::vector<std::vector<double>> generateAdjacencyMatrixWithDijkstra(const std::vector<Vertex<T>*>& pointsOfInterest, Vertex<T> * origin, Vertex<T> * destination);
+    std::vector<std::vector<double>> generateAdjacencyMatrixWithDijkstra(const std::vector<Vertex<T>*>& pointsOfInterest, Vertex<T> * start, Vertex<T> * finish);
 private:
     std::vector<Vertex<T>*> vertexSet;
 };
@@ -199,24 +199,22 @@ void Graph<T>::dijkstraShortestPath(const T& source) {
 }
 
 template <class T>
-std::vector<std::vector<double>> Graph<T>::generateAdjacencyMatrixWithDijkstra(const std::vector<Vertex<T>*>& pointsOfInterest, Vertex<T> * origin, Vertex<T> * destination) {
+std::vector<std::vector<double>> Graph<T>::generateAdjacencyMatrixWithDijkstra(const std::vector<Vertex<T>*>& pointsOfInterest, Vertex<T> * start, Vertex<T> * finish) {
     std::vector<std::vector<double>> adjacencyMatrix;
     std::vector<double> aux;
 
-    for (int i = 0; i < vertexSet.size() - 1; ++ i) {
-        if (vertexSet[i]->isPOI(pointsOfInterest) || vertexSet[i] == origin) {
-            dijkstraShortestPath(vertexSet[i]->getInfo());
-            for (int j = 0; j < vertexSet.size() - 1; ++ j) {
-                if (vertexSet[j]->isPOI(pointsOfInterest)) {
-                    aux.push_back(vertexSet[j]->getDist());
-                }
-                if (vertexSet[j] == origin) {
-                    aux.push_back(destination->getDist());
-                }
+    for (int i = 0; i < pointsOfInterest.size() - 1; ++ i) {
+        dijkstraShortestPath(vertexSet[i]->getInfo());
+        for (int j = 0; j < vertexSet.size() - 1; ++ j) {
+            if (vertexSet[j]->isPOI(pointsOfInterest)) {
+                aux.push_back(vertexSet[j]->getDist());
             }
-            adjacencyMatrix.push_back(aux);
-            aux.clear();
+            else if (vertexSet[j] == start) {
+                aux.push_back(finish->getDist());
+            }
         }
+        adjacencyMatrix.push_back(aux);
+        aux.clear();
     }
     return adjacencyMatrix;
 }
