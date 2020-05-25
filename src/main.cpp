ba11ad34d152d0b7584713bd6584ae38ad800c97
Goncalo Pascoal <goncalojpascoal@gmail.com>
2020-05-25 01:10:46 +0100

    Added error checking for working directory

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
+#include <fstream>
+#include <iostream>
+
 #include "menu.h"
 
 int main() {
+    std::ifstream ifs;
+    ifs.open("maps/4x4/nodes.txt");
+
+    if (ifs.fail()) {
+        std::cerr << "Please set your working directory to point to the src/ folder!" << std::endl;
+        return 1;
+    }
+
     menu::menuLoop();
     return 0;
 }
